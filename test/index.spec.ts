4978381e00d70786f6e9b70af3442a0c3780646b
Mohammed Al-Juboori <mhdaljobory@gmail.com>
2022-10-29 19:19:51 +0300

    Add test cases

---
+++ test/index.spec.ts
@@ -0,0 +1,391 @@
+import { deltaToMdConverter } from '../src/index'
+
+describe('Text', () => {
+    test('Bold, Italic, Strike and Link', () => {
+        const md = deltaToMdConverter.deltaToMarkdown([
+            {
+                "attributes": {
+                    "bold": true
+                },
+                "insert": "Bold"
+            },
+            {
+                "insert": " "
+            },
+            {
+                "attributes": {
+                    "italic": true
+                },
+                "insert": "Italic"
+            },
+            {
+                "insert": " "
+            },
+            {
+                "attributes": {
+                    "strike": true
+                },
+                "insert": "Strike"
+            },
+            {
+                "insert": " "
+            },
+            {
+                "attributes": {
+                    "link": "http://link.com"
+                },
+                "insert": "Link"
+            },
+            {
+                "insert": "\n"
+            }
+        ]);
+
+        expect(md).toBe("**Bold** _Italic_ ~~Strike~~ [Link](http://link.com)");
+    });
+
+    test('Heading', () => {
+        const md = deltaToMdConverter.deltaToMarkdown([
+            {
+                "insert": "Head 1"
+            },
+            {
+                "attributes": {
+                    "header": 1
+                },
+                "insert": "\n"
+            },
+            {
+                "insert": "Head 2"
+            },
+            {
+                "attributes": {
+                    "header": 2
+                },
+                "insert": "\n"
+            },
+            {
+                "insert": "Head 3"
+            },
+            {
+                "attributes": {
+                    "header": 3
+                },
+                "insert": "\n"
+            }
+        ]);
+
+        expect(md).toBe("Head 1\n======\n\nHead 2\n------\n\n### Head 3");
+    });
+
+    test('Quote, Code And Code block', () => {
+        const md = deltaToMdConverter.deltaToMarkdown([
+            {
+                "insert": "Quote"
+            },
+            {
+                "attributes": {
+                    "blockquote": true
+                },
+                "insert": "\n"
+            },
+            {
+                "attributes": {
+                    "code": true
+                },
+                "insert": "Code"
+            },
+            {
+                "insert": "\nCode Block"
+            },
+            {
+                "attributes": {
+                    "code-block": true
+                },
+                "insert": "\n"
+            }
+        ]);
+
+        expect(md).toBe("> Quote\n\n`Code`\n\n    Code Block");
+    });
+
+    test('Bullet List', () => {
+        const md = deltaToMdConverter.deltaToMarkdown([
+            {
+                "insert": "Level 1"
+            },
+            {
+                "attributes": {
+                    "list": "bullet"
+                },
+                "insert": "\n"
+            },
+            {
+                "insert": "Level 1 - 1"
+            },
+            {
+                "attributes": {
+                    "indent": 1,
+                    "list": "bullet"
+                },
+                "insert": "\n"
+            },
+            {
+                "insert": "Level 1 - 1 - 1"
+            },
+            {
+                "attributes": {
+                    "indent": 2,
+                    "list": "bullet"
+                },
+                "insert": "\n"
+            },
+            {
+                "insert": "Level 1 - 1 - 2"
+            },
+            {
+                "attributes": {
+                    "indent": 2,
+                    "list": "bullet"
+                },
+                "insert": "\n"
+            },
+            {
+                "insert": "Level 1 - 2"
+            },
+            {
+                "attributes": {
+                    "indent": 1,
+                    "list": "bullet"
+                },
+                "insert": "\n"
+            },
+            {
+                "insert": "Level 2"
+            },
+            {
+                "attributes": {
+                    "list": "bullet"
+                },
+                "insert": "\n"
+            },
+            {
+                "insert": "Level 2 - 1"
+            },
+            {
+                "attributes": {
+                    "indent": 1,
+                    "list": "bullet"
+                },
+                "insert": "\n"
+            },
+            {
+                "insert": "Level 3"
+            },
+            {
+                "attributes": {
+                    "list": "bullet"
+                },
+                "insert": "\n"
+            },
+            {
+                "insert": "Level 4"
+            },
+            {
+                "attributes": {
+                    "list": "bullet"
+                },
+                "insert": "\n"
+            }
+        ]);
+
+        expect(md).toBe("* Level 1\n\n    * Level 1 - 1\n\n        * Level 1 - 1 - 1\n\n        * Level 1 - 1 - 2\n\n    * Level 1 - 2\n\n* Level 2\n\n    * Level 2 - 1\n\n* Level 3\n\n* Level 4");
+    });
+
+    test('Ordered List', () => {
+        const md = deltaToMdConverter.deltaToMarkdown([
+            {
+                "insert": "Level 1"
+            },
+            {
+                "attributes": {
+                    "list": "ordered"
+                },
+                "insert": "\n"
+            },
+            {
+                "insert": "Level 1 - 1"
+            },
+            {
+                "attributes": {
+                    "indent": 1,
+                    "list": "ordered"
+                },
+                "insert": "\n"
+            },
+            {
+                "insert": "Level 1 - 1 - 1"
+            },
+            {
+                "attributes": {
+                    "indent": 2,
+                    "list": "ordered"
+                },
+                "insert": "\n"
+            },
+            {
+                "insert": "Level 1 - 1 - 2"
+            },
+            {
+                "attributes": {
+                    "indent": 2,
+                    "list": "ordered"
+                },
+                "insert": "\n"
+            },
+            {
+                "insert": "Level 1 - 2"
+            },
+            {
+                "attributes": {
+                    "indent": 1,
+                    "list": "ordered"
+                },
+                "insert": "\n"
+            },
+            {
+                "insert": "Level 2"
+            },
+            {
+                "attributes": {
+                    "list": "ordered"
+                },
+                "insert": "\n"
+            },
+            {
+                "insert": "Level 2 - 1"
+            },
+            {
+                "attributes": {
+                    "indent": 1,
+                    "list": "ordered"
+                },
+                "insert": "\n"
+            },
+            {
+                "insert": "Level 3"
+            },
+            {
+                "attributes": {
+                    "list": "ordered"
+                },
+                "insert": "\n"
+            },
+            {
+                "insert": "Level 4"
+            },
+            {
+                "attributes": {
+                    "list": "ordered"
+                },
+                "insert": "\n"
+            }
+        ]);
+
+        expect(md).toBe("1. Level 1\n\n    1. Level 1 - 1\n\n        1. Level 1 - 1 - 1\n\n        2. Level 1 - 1 - 2\n\n    2. Level 1 - 2\n\n2. Level 2\n\n    1. Level 2 - 1\n\n3. Level 3\n\n4. Level 4");
+    });
+
+    test('Ordered And Bullet List', () => {
+        const md = deltaToMdConverter.deltaToMarkdown([
+            {
+                "insert": "Ordered 1"
+            },
+            {
+                "attributes": {
+                    "list": "ordered"
+                },
+                "insert": "\n"
+            },
+            {
+                "insert": "Bullet 1"
+            },
+            {
+                "attributes": {
+                    "indent": 1,
+                    "list": "bullet"
+                },
+                "insert": "\n"
+            },
+            {
+                "insert": "Bullet 2"
+            },
+            {
+                "attributes": {
+                    "indent": 1,
+                    "list": "bullet"
+                },
+                "insert": "\n"
+            },
+            {
+                "insert": "Ordered 1"
+            },
+            {
+                "attributes": {
+                    "list": "ordered"
+                },
+                "insert": "\n"
+            },
+            {
+                "insert": "Ordered 2"
+            },
+            {
+                "attributes": {
+                    "list": "ordered"
+                },
+                "insert": "\n"
+            },
+            {
+                "insert": "\n"
+            }
+        ]);
+
+        expect(md).toBe("1. Ordered 1\n\n    * Bullet 1\n\n    * Bullet 2\n\n1. Ordered 1\n\n2. Ordered 2");
+    });
+
+    test('Code Block After List', () => {
+        const md = deltaToMdConverter.deltaToMarkdown([
+            {
+                "insert": "Ordered 1"
+            },
+            {
+                "attributes": {
+                    "list": "ordered"
+                },
+                "insert": "\n"
+            },
+            {
+                "insert": "Ordered 2"
+            },
+            {
+                "attributes": {
+                    "list": "ordered"
+                },
+                "insert": "\n"
+            },
+            {
+                "insert": "Code Block"
+            },
+            {
+                "attributes": {
+                    "code-block": true
+                },
+                "insert": "\n"
+            },
+            {
+                "insert": "\n\n"
+            }
+        ]);
+
+        expect(md).toBe("1. Ordered 1\n\n2. Ordered 2\n\n    Code Block");
+    });
+});
