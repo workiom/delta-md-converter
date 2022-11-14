6ea6131dee29e470be97eadbbed7cb3d98129c80
Mohammed Al-Juboori <mhdaljobory@gmail.com>
2022-11-14 15:18:11 +0300

    Remove gulp

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { deltaToMarkdown } from './delta-to-markdown';
 import { markdownToDelta } from './markdown-to-delta';
 
-export const deltaToMdConverter = {
+export {
     deltaToMarkdown,
     markdownToDelta
 };
