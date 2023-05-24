ac4a38ef6cd5a2686313e98efd04480f39fffec8
Mohammed Al-Juboori <mhdaljobory@gmail.com>
2023-05-24 15:32:43 +0300

    Remove compiled files and add deploy command

--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare const _default: {
-    deltaToMarkdown: (ops: any, mentions?: import("./delta-to-markdown").IDeltaMention[] | undefined) => string;
-    markdownToDelta: (md: string, mentions?: import("./markdown-to-delta").IStringMention[] | undefined) => any;
-};
-export default _default;
